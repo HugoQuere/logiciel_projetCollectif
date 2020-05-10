954a8e32c177691f2bd25258175cf851c36e1404
Djawad <djawad.mdallah-mari@etu.univ-tours.fr>
2020-05-10 16:40:42 +0200

    Ajout de jeu de test pour les problèmes

---
+++ requeteSQL/jeuDeTest/createTableProblemeSysteme.sql
@@ -0,0 +1,15 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  d-dja
+ * Created: 10 mai 2020
+ */
+
+INSERT INTO PROBLEMESYSTEME (idCategorieProbleme,commentaire,dateCreation,dateResolution )
+    VALUES
+        (3,'Camera non détécté','2020-05-10',NULL),
+        (3,'Plantage du logiciel','2020-05-10',NULL),
+        (3,'Bug sur le traitement d image','2020-05-10',NULL);
---
+++ requeteSQL/jeuDeTest/jeuDeTestProblemePalmipede.sql
@@ -0,0 +1,15 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  d-dja
+ * Created: 10 mai 2020
+ */
+
+INSERT INTO PROBLEMEPALMIPEDE (idPalmipede,idCategorieProbleme,commentaire,dateCreation,dateResolution )
+    VALUES
+        (1,2,'Trop de ponte en peu de temps','2020-05-10',NULL),
+        (2,2,'N apparait jamais','2020-05-10',NULL),
+        (3,2,'Pas de ponte depuis 2 mois','2020-04-10','2020-05-10');
