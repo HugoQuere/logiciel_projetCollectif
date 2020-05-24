4da55ccd2145fc221db223315deafaac554f3024
Hugo Quere <hugo.quere@etu.univ-tours.fr>
2020-05-24 23:13:45 +0200

    Modification légére du jeu de test pour avoir quelque informations

--- requeteSQL/jeuDeTest/jeuDeTestPonte.sql
+++ requeteSQL/jeuDeTest/jeuDeTestPonte.sql
@@ -10,8 +10,8 @@
 
 INSERT INTO PONTE (idPalmipede, idNid, datePonte, heureDebutPonte, heureFinPonte, precenseOeuf, oeufCollecte)
     VALUES
-            (2, 1, '2020-05-01', '16:32:22', '16:42:22', TRUE, FALSE),
-            (2, 1, '2020-03-22', '17:32:22', '18:32:22', FALSE, FALSE),
+            (2, 1, '2020-05-23', '16:32:22', '16:41:12', TRUE, FALSE),
+            (2, 1, '2020-05-24', '17:25:10', '18:22:22', FALSE, FALSE),
             (2, 2, '2020-03-22', '12:32:22', '16:32:22', TRUE, FALSE),
             (3, 1, '2020-03-22', '13:32:22', '14:32:22', FALSE, FALSE),
-            (3, 1, '2020-04-12', '15:28:22', '15:45:22', TRUE, FALSE);
+            (3, 1, '2020-05-24', '15:28:22', '15:45:22', TRUE, FALSE);
